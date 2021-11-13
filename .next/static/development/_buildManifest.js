cee67f3adea05158877a73a63361f6ec6d32e427
Abhishek <abhishk.charpale@walcandsangli.ac.in>
2021-11-13 10:57:59 +0530

    .next hide 3

--- .next/static/development/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/liveelection":["static\u002Fchunks\u002Fpages\u002Fliveelection.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fliveelection"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/[admin]/addelectionscreen":["static\u002Fchunks\u002Fpages\u002F[admin]\u002Faddelectionscreen.js"],"/[admin]/electionscreen":["static\u002Fchunks\u002Fpages\u002F[admin]\u002Felectionscreen.js"],sortedPages:["\u002F_app","\u002F_error","\u002F[admin]\u002Faddelectionscreen","\u002F[admin]\u002Felectionscreen"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 56877c8b08db9f881bd9298ccf4ca2c6efbc8eb1

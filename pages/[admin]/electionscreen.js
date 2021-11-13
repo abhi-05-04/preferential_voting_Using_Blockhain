86a3d4842bbe510c61c79e216caf0c36a9a7dd42
Abhishek <abhishk.charpale@walcandsangli.ac.in>
2021-11-13 11:59:19 +0530

    changed styling in electionscreen

--- pages/[admin]/electionscreen.js
+++ pages/[admin]/electionscreen.js
@@ -9,27 +9,12 @@ export default function electionscreen() {
             <div className="container" style={{ marginTop: "50px" }}>
                 <Head />
                 <Nav />
-            
-            <div class="row" style={{ marginTop: "10px", marginBottom: "10px" }}>
-                <div class="col">
-                </div>
-                <div class="col">
-                    <div class="row">
-                        <div class="col text-white" style={{ fontSize: "30px", verticalAlign: "middle" }}>
-                            Elections Name
-                        </div>
-                        {/* <div class="col" style={{verticalAlign: "middle"}}>
-                            <button type="submit" className="btn btn-primary btn-block">
-                                <a className="nav-link" href="/admin/addelectionscreen" style={{color: "White"}}>Add</a>
-                            </button>
-                        </div> */}
+
+                    <div class="container text-white mb-3 w-50" style={{ fontSize: "30px", verticalAlign: "middle" }}>
+                        Elections Name
                     </div>
-                </div>
-                <div class="col">
-                </div>
-            </div>
-            <div class="row">
-              {/*   <div class="col">
+                {/*<div class="row">
+                       <div class="col">
                 </div>
                 <div class="col">
                     <div>
@@ -85,24 +70,24 @@ export default function electionscreen() {
                     </div>
                 </div>
                 <div class="col">
-                </div>*/}
-            </div> 
-            <div class="input-group mb-3 w-50 container">
-                <input type="text" class="form-control" placeholder="Add Candidate name" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-                <button class ="btn btn-outline-secondary text-white" type ="button" id="button-addon2">Add Candidate</button>
             </div>
-            <div class="input-group mb-3 w-50 container">
-                <input type="text" class="form-control" placeholder="voters metamask adddress" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-                <button class ="btn btn-outline-secondary text-white" type ="button" id="button-addon2">Give Voting Rights</button>
-            </div>
-            <div class="input-group mb-3 w-50 container" >
-                {/* <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/> */}
-                <button class ="btn btn-outline-secondary text-white" type ="button" id="button-addon2">Calculate Votes</button>
-            </div>
-            <div class="input-group mb-3 w-50 container">
-                {/* <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/> */}
-                <button class ="btn btn-outline-secondary text-white" type ="button" id="button-addon2">Check Majority</button>
-            </div>
+                </div> */}
+                <div class="input-group mb-3 w-50 container">
+                    <input type="text" class="form-control" placeholder="Add Candidate name" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button class="btn btn-outline-secondary text-white" type="button" id="button-addon2">Add Candidate</button>
+                </div>
+                <div class="input-group mb-3 w-50 container">
+                    <input type="text" class="form-control" placeholder="voters metamask adddress" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button class="btn btn-outline-secondary text-white" type="button" id="button-addon2">Give Voting Rights</button>
+                </div>
+                <div class="input-group mb-3 w-50 container" >
+                    {/* <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/> */}
+                    <button class="btn btn-outline-secondary text-white" type="button" id="button-addon2">Calculate Votes</button>
+                </div>
+                <div class="input-group mb-3 w-50 container">
+                    {/* <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/> */}
+                    <button class="btn btn-outline-secondary text-white" type="button" id="button-addon2">Check Majority</button>
+                </div>
             </div>
         </div>
     )
--- pages/verify.js
+++
@@ -1,9 +0,0 @@
-import { connectToDatabase } from "../lib/";
-
-export default async (req, res) => {
-  const { db } = await connectToDatabase();
-  const data = req.query;
-  const response = await db.collection("xyz").insertOne(data);
-  
-  res.json(response);  
-};
